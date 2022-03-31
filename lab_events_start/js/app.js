3a13bb2f8bba04491d027b27d5c7b1bbf6f62d85
Dan7-7-7 <dannyranson19@gmail.com>
2022-03-31 15:07:22 +0100

    functionality completed, working on css

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  const button = document.querySelector('#delete');
+  button.addEventListener('click', handleButtonClick);
 })
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+  const newBook = document.createElement('ul');
+  newBook.id = 'item';
+
+  const title = document.createElement('li');
+  const author = document.createElement('li');
+  const category = document.createElement('li');
+  title.id = 'title';
+  author.id = 'author';
+  category.id = 'category';
+  newBook.appendChild(title);
+  newBook.appendChild(author);
+  newBook.appendChild(category);
+  title.textContent = `Title: ${event.target.title.value}` ;
+  author.textContent = `Author: ${event.target.author.value}`;
+  category.textContent = `Category: ${event.target.category.value}`;
+
+  const list = document.querySelector('#reading-list');
+  list.appendChild(newBook)
+  document.querySelector('#new-item-form').reset();
+}
+
+const handleButtonClick = function(event){
+  const deletedBooks = document.querySelectorAll('#item');
+  const deletedArray = Array.from(deletedBooks);
+  deletedArray.forEach((book) => {
+    book.remove();
+})
+}
